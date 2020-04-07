006bc3fb6e5a209210d17fc5040b2914b1516f77
Rafael Arthur Rocha Miranda <43150739+rafael-miranda10@users.noreply.github.com>
2020-04-07 10:46:25 -0300

    Commit inicial do projeto

---
+++ FaceDetection-windowsForms/FaceDetection/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FaceDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.cboDevice = new System.Windows.Forms.ComboBox();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(537, 15);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(75, 39);
+            this.btnDetect.TabIndex = 0;
+            this.btnDetect.Text = "Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // cboDevice
+            // 
+            this.cboDevice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboDevice.FormattingEnabled = true;
+            this.cboDevice.Location = new System.Drawing.Point(114, 23);
+            this.cboDevice.Name = "cboDevice";
+            this.cboDevice.Size = new System.Drawing.Size(371, 24);
+            this.cboDevice.TabIndex = 1;
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(30, 73);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(582, 569);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Device:";
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(633, 73);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(582, 569);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 4;
+            this.pic2.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(1240, 73);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(582, 569);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 5;
+            this.pic3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1839, 663);
+            this.Controls.Add(this.pic3);
+            this.Controls.Add(this.pic2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.cboDevice);
+            this.Controls.Add(this.btnDetect);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Face Detection";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.ComboBox cboDevice;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+    }
+}
+
---
+++ FaceDetection-windowsForms/FaceDetection/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace FaceDetection
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection filter;
+        VideoCaptureDevice device;
+        VideoCapture capture = new VideoCapture(); //create a camera capture
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt_tree.xml");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDetect_Click(object sender, System.EventArgs e)
+        {
+            device = new VideoCaptureDevice(filter[cboDevice.SelectedIndex].MonikerString);
+            device.NewFrame += Device_newFrame;
+            device.Start();
+        }
+
+        private void Device_newFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            //Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            //Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 1.2, 1);
+            //foreach (Rectangle rectangle in rectangles)
+            //{
+            //    using (Graphics graphics = Graphics.FromImage(bitmap))
+            //    {
+            //        using (Pen pen = new Pen(Color.Red, 5))
+            //        {
+            //            graphics.DrawRectangle(pen, rectangle);
+            //        }
+            //    }
+            //}
+            //pic.Image = bitmap;
+
+
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> originalImage = new Image<Bgr, byte>(bitmap);
+            Image<Bgr, byte> GaussianImage = originalImage.SmoothGaussian(11);
+            Image<Gray, byte> originalImgGray = originalImage.Convert<Gray, byte>();
+            Image<Gray, byte> gaussianImgGray = GaussianImage.Convert<Gray, byte>();
+
+
+            //Canny
+            // Image<Gray, byte> originalFilteredImage = originalImgGray.Canny(20, 50);
+            // Image<Gray, byte> gaussianFilteredImage = gaussianImgGray.Canny(20, 50);
+
+            //Sobel
+            //Image<Gray, float> originalFilteredImage = originalImgGray.Sobel(1, 0, 5);
+            //Image<Gray, float> gaussianFilteredImage = originalImgGray.Sobel(1, 0, 3).Sobel(0,1,3).AbsDiff(new Gray(0.0));
+
+            //Laplace
+            //Image<Gray, float> originalFilteredImage = originalImgGray.Laplace(9);
+            //Image<Gray, float> gaussianFilteredImage = originalImgGray.Laplace(7);
+
+
+            //ThresholdBinary
+            //Image<Gray, byte> originalFilteredImage = originalImgGray.ThresholdBinary(new Gray(45), new Gray(255));
+            //Image<Gray, byte> gaussianFilteredImage = gaussianImgGray.ThresholdBinary(new Gray(30), new Gray(255));
+
+
+            Image<Gray, byte> originalFilteredImage = new Image<Gray, byte>(originalImgGray.Width, originalImgGray.Height, new Gray(0));
+            Image<Gray, byte> gaussianFilteredImage = new Image<Gray, byte>(gaussianImgGray.Width, gaussianImgGray.Height, new Gray(0));
+            CvInvoke.Threshold(originalImgGray, originalFilteredImage, 125, 255,Emgu.CV.CvEnum.ThresholdType.Otsu);
+            CvInvoke.Threshold(gaussianImgGray, gaussianFilteredImage, 125, 255, Emgu.CV.CvEnum.ThresholdType.Otsu);
+
+            // https://stackoverflow.com/questions/25989754/what-is-the-function-to-find-otsu-threshold-in-emgu-cv
+
+
+
+
+
+            pic.Image = originalImage.Bitmap;
+            pic2.Image = originalFilteredImage.Bitmap;
+            pic3.Image = gaussianFilteredImage.Bitmap;
+
+
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            filter = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filter)
+            {
+                cboDevice.Items.Add(device.Name);
+            }
+            cboDevice.SelectedIndex = 0;
+            device = new VideoCaptureDevice();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (device.IsRunning)
+            {
+                device.Stop();
+            }
+        }
+    }
+}
